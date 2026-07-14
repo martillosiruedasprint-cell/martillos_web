a2d81d5a6f63f661af19bcd21301c9011dceb0e9
martillosiruedasprint-cell <martillosiruedasprint@gmail.com>
2026-07-14 09:58:04 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask, render_template, request, session, redirect, jsonify
+import sqlite3
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY', 'martillos_seguridad_alta_2026')
+
+# ... (resto de tu lógica que ya tenemos) ...
+if __name__ == '__main__':
+    app.run()
